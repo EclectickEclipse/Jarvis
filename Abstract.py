2f65df5ed3d08f45b415d4e4d4befe32b8557905
EclectickEclipse <ariana.giroux@gmail.com>
2016-04-16 03:34:18 -0600

    Priority Assesment.

--- Abstract.py
+++ Abstract.py
@@ -1,11 +1,12 @@
 # /usr/local/bin python
 
+
 class Definitions(object):
     definitions = {
         'App': 'A user interaction script.'
     }
 
 # TODO Further define application launching.
-# TODO Research how frameworks like Django run python applications.
+# TODO Research how frameworks like Django run python scripts.
 # TODO Determine if Jarvis should be able to execute non Pythonic code. (i.e *NIX compiled C like executables)
 # TODO Create functionality to pass terminal control to various "shell" like objects.
